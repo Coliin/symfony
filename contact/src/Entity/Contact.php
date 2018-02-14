0d0e9ab2d325303013fbe1b7e4158cf45c373671
Colin Thorel <21603567@etu.unicaen.fr>
2018-02-14 08:57:43 +0100

    Création de la base de données contact et ajout de doctrine

---
+++ contact/src/Entity/Contact.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $tel;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $mobile;
+}
